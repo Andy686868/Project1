e637521f600c99e03c5effb17061759319c19405
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-04 11:31:05 +0300

    lesson6-1-4

---
+++ l6-1-3.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+
+int main(void)
+{
+
+    char s;  
+    scanf("%c",&s);
+
+    switch (s) {
+        case 'f' : 
+            printf("и себя, и коня потеряешь!\n");break;
+        case 'l' : 
+            printf("коня потеряешь, себя спасёшь!\n"); break; 
+        case 'r' : 
+            printf("себя потеряешь, коня спасёшь!\n"); break;
+        default:
+            printf("");
+    }
+
+    return 0;
+}
