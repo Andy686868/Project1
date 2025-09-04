b2e1d9767d51a86a84c48ebf06239e74bc61e9cc
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-04 14:42:20 +0300

    lesson6-1-13

---
+++ l6-1-13.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+    int k, x;
+    scanf("%d %d",&k, &x);
+    switch (k)
+    {
+    case 1:
+        printf("%d",x);
+        break;
+    case 2:
+        printf("%d",(x/10)*(x%10));
+        break;
+    case 3: 
+        printf("%d",(x/100)*(x%10)*(x/10%10));
+        break;
+    case 4: 
+        printf("%d",(x/1000)*(x%10)*(x/100%10)*(x/10%10));
+        break;
+    
+    default:
+        break;
+    }
+    return 0;
+}
