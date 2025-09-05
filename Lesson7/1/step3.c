a7b193c4911de694ccbc83fb113eff2c6f1077f4
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-05 15:38:09 +0300

    Lesson 7.1Step2

---
+++ Lesson7/1/step3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+    int K, M;
+    scanf("%d %d",&K, &M);
+    for (K; K<=M; K++){
+        if (K>0){
+            printf("%d ",K);
+        }
+    }
+    return 0;
+}
