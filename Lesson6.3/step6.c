61d840a28e94cd7c31dc7f1970725d44df163b49
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-05 14:48:59 +0300

    Lesson 6.3.Step6

---
+++ Lesson6.3/step6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d",&a, &b, &c, &d);
+    if (a == 1 && b == 0 && c == 2 && d == 4){
+        printf("open");
+    }
+    else{
+        printf("closed");
+    }
+    return 0;
+}
