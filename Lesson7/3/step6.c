6c32e31ebbdda63462c26c23c43cb56aec8f5cd2
Andrey Nikitin <nikitin.andrux@yandex.ru>
2025-09-07 10:22:42 +0300

    Lesson 7.3 Step 6

---
+++ Lesson7/3/step6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int N,g=-9999,flag = 0;
+    scanf("%d",&N);
+    while(N != -9999){
+        if (N<=g){
+            flag += 1;
+        }
+        g = N;
+        scanf("%d",&N);
+    }
+    if (flag == 0){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+}
