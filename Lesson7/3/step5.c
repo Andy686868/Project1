5e80dfec0d33a14ee0d49eddd015048987aef47d
Andrey Nikitin <nikitin.andrux@yandex.ru>
2025-09-07 10:14:40 +0300

    Lesson 7.3 Step 5

---
+++ Lesson7/3/step5.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int N,num;
+    int g = -9999999;
+    scanf("%d",&N);
+    for (int i = 1; i<=N; i++){
+        scanf("%d",&num);
+        if (num != g){
+            printf("%d ",num);
+        }
+        g = num;
+
+    }
+    return 0;
+}
