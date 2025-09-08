c67171bbfa94418323e96a7c8ba2b402e461d84a
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-08 17:00:51 +0300

    Home 2

---
+++ Lesson8/2/step10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int N;
+    scanf("%d",&N);
+    int matrix[N][N];
+    for ( int  i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            scanf("%d",&matrix[i][j]);
+        }
+    }
+    int s;
+    scanf("%d",&s);
+    for(int a = 0; a < N; a++){
+        for(int b = 0; b < N; b++){
+            printf("%d ",matrix[a][(b+s)%(N-1)]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
