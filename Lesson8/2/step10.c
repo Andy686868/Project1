13760ebebed35fcac73326b576c1c39b3c9243e2
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-09 08:47:27 +0300

    Lesson 8.2 Step 9

--- Lesson8/2/step10.c
+++ Lesson8/2/step10.c
@@ -11,9 +11,13 @@ int main(){
     }
     int s;
     scanf("%d",&s);
+    int shift = s % N;
+    if (shift < 0) {
+        shift += N;  
+    }
     for(int a = 0; a < N; a++){
         for(int b = 0; b < N; b++){
-            printf("%d ",matrix[a][(b+s)%(N-1)]);
+            printf("%d ",matrix[a][(b-shift+N)%(N)]);
         }
         printf("\n");
     }
