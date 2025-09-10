cfc53fbf3b4a149639f345a2b876d18aae82332d
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-10 16:42:28 +0300

    Lesson 9.2 : step 6

--- Lesson9/2/step6.c
+++ Lesson9/2/step6.c
@@ -1,1 +1,18 @@
-Sas
+#include <stdio.h>
+
+int main(){
+    char str[102];
+    fgets(str,102,stdin);
+    int count = 0;
+    while (str[count] != '\0')
+    {
+        if (str[count] == ' ' && str[count+1] != ' '&& count != 0){
+            printf("%c",str[count]);
+        }
+        else if(str[count] != ' '){
+            printf("%c",str[count]);
+        }
+        count++;
+    }
+    return 0;
+}
