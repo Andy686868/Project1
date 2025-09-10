eff6917c80c6839342a63a2998d84be058ed7361
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-10 14:46:04 +0300

    Lesson 9.2 step 3

--- Lesson9/2/step3.c
+++ Lesson9/2/step3.c
@@ -1,20 +1,49 @@
-#include <stdio.h>
+/*#include <stdio.h>
 
 int main(){
     int c;
-    int count = 1, flag = 0;
+    int count = 0, flag = 1;
     while ((c = getchar())!='\n' && c != '\0'){
-        if (c == ' '){
-            if (flag == 0){
+        if (c != ' '){
+            if (flag == 1){
                 count++;
                 flag += 1;
             } 
         }
         else {
-            flag = 0;
+            flag = 1;
         }
     }
     printf("%d", count);
     return 0;
     
+}
+
+#include <stdio.h>
+
+int main(){
+    char ch[101];
+    int count = 1;
+    fgetc(ch);
+    while (ch[count]!='\n')
+    {
+        scanf("%s",&ch);
+        count++;
+    }
+    printf("%d",count);
+    return 0;
+}*/
+#include <stdio.h>
+
+int main() {
+    int n = 0, i = 0;
+    char str[100];
+    gets(str);
+    while(str[i] != '\0'){
+         if(i==0 && str[i] != ' ') n++;
+         else if (str[i] == ' ' && str[i] != str[i+1] && str[i+1] != '\0') n++;
+         i+=1;     
+     }
+    printf("%d", n);
+    return 0;
 }
