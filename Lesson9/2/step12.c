3a9ea3e08b348fb734580ef096830d06a2f66f58
Андрей Вячеславович Никитин <_@almaz.localnet>
2025-09-18 16:29:33 +0300

    Lesson 9.2 : step 12

--- Lesson9/2/step12.c
+++ Lesson9/2/step12.c
@@ -7,50 +7,19 @@ int main(){
     int sc = 0;
     char c;
     fgets(str,53,stdin);
-    while (str[count] != '\0' ){//|| str[count]!='\n'){
+    while (str[count] != '\0' ){
         c = str[count];
-        //printf("%c\n",str[count]);
         fr = count;
-
-        while (!(str[fr]=='\0' || str[fr]==' ' || str[fr]=='\n'))
-        {
-            fr++;
+        while(!(str[count]=='\0'|| str[count]=='\n'|| str[count]==' ')){
+            count++;
         }
-        fr--;
-        if(str[fr] == str[count] && fr != count){
-            printf("%c=%d - %d\n",str[fr],count,fr);
-            while (count < fr){
-                printf("%c",str[count]);
-                count++;
+        if (str[fr]==str[count-1]){
+            for (;fr < count;fr++){
+                printf("%c",str[fr]);
             }
             printf(" ");
         }
         count++;
-
-        
-
-        
     }
-}
-/*
-#include <stdio.h>
-
-int main(){
-    char N[53];
-    char M[53];
-    fgets(M,53,stdin);
- 
-    {
-
-    }
-    
-    while (scanf("%s",N)==1)
-    {
-        if (N[0] == N[-1]){
-            printf("%s",N);
-        }
-    }
-
     return 0;
-    
-}*/
+}
