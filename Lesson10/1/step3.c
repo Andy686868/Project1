e825d482503cae0113ce1e2b33f25a8fc56768e9
Andrey Nikitin <nikitin.andrux@yandex.ru>
2025-09-18 21:54:55 +0300

    Lesson 9.2 : step 3

---
+++ Lesson10/1/step3.c
@@ -0,0 +1,14 @@
+/* Напишите здесь заголовок и тело функции sum. */
+int min(int a, int b){
+    int result;
+    if (a>b){
+        result = b;
+    }
+    else {
+        result = a;
+    }
+    return result;
+}
+
+
+
